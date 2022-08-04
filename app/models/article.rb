d42a0952bd459bd2cd829e1d92168765887e632f
haris <harisishfaq07@gmail.com>
2022-08-04 15:18:29 +0500

    photo uploading

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
 belongs_to :user
 has_many :article_categories
 has_many :categories , through: :article_categories
+# has_rich_text :description
     validates :name , presence: true 
     validates :description , presence: true 
 
