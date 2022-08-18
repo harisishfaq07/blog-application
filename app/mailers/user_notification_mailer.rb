b88b870de38892f5ce565fcd68556921791343cb
haris <harisishfaq07@gmail.com>
2022-08-18 12:38:18 +0500

    change mailer

--- app/mailers/user_notification_mailer.rb
+++ app/mailers/user_notification_mailer.rb
@@ -4,10 +4,10 @@ class UserNotificationMailer < ApplicationMailer
   def create_article(object)
 # @usr = params[:user]
 
-   @user = object
-   @useremail = User.find(@user.user_id)
+   @article = object
+   @useremail = User.find(@article.user_id)
 
-    mail to: @useremail.email , subject: "A new user has been created now. At #{@user.created_at}"
+    mail to: @useremail.email , subject: "A new user has been created now. At #{@article.created_at}"
     
   end
 
